7493fa1fe9a0c752652c10be9c8ee7bd6ad8edcf
gremlin961 <richard.kiles@gmail.com>
2023-11-06 08:49:57 -0500

    First test run

--- main.py
+++ main.py
@@ -6,12 +6,15 @@
 # typing - Primarliy using the Union function to handle optional json data passed to the API
 # GenImage - Custom library used to manage the generated AI images and layer them for the final banner
 
-from fastapi import FastAPI, File, UploadFile, Request
+from fastapi import FastAPI, File, UploadFile, Request, HTTPException
 import uvicorn
 import shutil
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import json
+from typing import List
+from pkg import convert
+
 
 
 app = FastAPI()
@@ -23,10 +26,29 @@ async def root(request: Request):
    return templates.TemplateResponse("uploadfile.html", {"request": request})
 
 
-@app.post("/schema/")
-def upload_file(file: UploadFile):
-    if file.content_type != "application/json":
-        raise HTTPException(400,detail="Invalid document type")
-    else:
-        data = json.loads(file.file.read())
-    return {"content":data ,"filename":file.filename}
+@app.post("/convert")
+async def upload_multiple_files(files: List[UploadFile] = File(...)):
+    for file in files:
+        if file.content_type != "application/json":
+            raise HTTPException(400,detail="Invalid document type for file "+file.filename+". Only json files are allowed")
+            
+    parameters = json.loads(files[0].file.read())
+    template = json.loads(files[1].file.read())
+    data = json.loads(files[2].file.read())
+
+    converted_data = convert.GetData(parameters, template, data)
+
+    return converted_data
+    #return response
+
+
+
+#@app.post("/convert")
+#def upload_file(files: list[UploadFile]):
+    #if file.content_type != "application/json":
+        #raise HTTPException(400,detail="Invalid document type")
+    #else:
+        #data = json.loads(file.file.read())
+    #return {"content":data ,"filename":file.filename}
+
+
---
+++ pkg/convert.py
@@ -0,0 +1,40 @@
+import vertexai
+from vertexai.language_models import TextGenerationModel
+import json
+
+
+def GetData(PARAMETERS, TEMPLATE, DATA):
+
+	project_id = PARAMETERS['project_id']
+	location = PARAMETERS['location']
+	model = PARAMETERS['model']
+	c_count = PARAMETERS['parameters']['candidate_count']
+	max_output = PARAMETERS['parameters']['max_output_tokens']
+	temp = PARAMETERS['parameters']['temperature']
+	top_p = PARAMETERS['parameters']['top_p']
+	top_k = PARAMETERS['parameters']['top_k']
+	context = PARAMETERS['context']
+	prompt = PARAMETERS['prompt']
+
+
+
+	vertexai.init(project=project_id, location=location)
+	parameters = {
+	    "candidate_count": c_count,
+	    "max_output_tokens": max_output,
+	    "temperature": temp,
+	    "top_p": top_p,
+	    "top_k": top_k
+	}
+	model = TextGenerationModel.from_pretrained(model)
+	response = model.predict(
+		""""""+context+
+		""""""+json.dumps(TEMPLATE)+
+		"""input: """+prompt+
+		""""""+json.dumps(DATA)+
+		"""output:
+		""",
+			**parameters
+	)
+	#converted_data = top_p
+	return response.text
